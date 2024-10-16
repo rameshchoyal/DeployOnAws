0f959370794915256991a6e6521467f84b98e3dd
Ramesh <72539623+rameshchoyal@users.noreply.github.com>
2024-10-16 20:17:57 +0530

    Update index.js checking ci/cd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello There! next update : It is updated from my terminal now added .yaml");
+  res.send("Hello there from github ci/cd piple help");
 });
 
 app.listen(port, () => {
