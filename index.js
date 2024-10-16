66d8ff865bd54c2039298bed5398c6c4ea392b72
Ramesh <72539623+rameshchoyal@users.noreply.github.com>
2024-10-16 17:37:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Now updating from own terminal");
+  res.send("Hello World! Now updating from own terminal 2");
 });
 
 app.listen(port, () => {
